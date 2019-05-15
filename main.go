1d9aa061231ba2a8cbd4d846a5ffbdff62595e70
Mateusz Greń <mateusz.gren1992@gmail.com>
2019-05-15 20:14:36 +0200

    remove unnecessary comments

--- main.go
+++ main.go
@@ -14,11 +14,3 @@ func main() {
 	defer memguard.DestroyAll()
 	cmd.Execute()
 }
-
-// https://goreportcard.com/about/ + release + license
-// https://blitiri.com.ar/git/r/chasquid/b/master/t/f=Makefile.html
-// test z block device
-// rebuild profile ze zmianą hasła / kluczy
-// wytestować jeszcze raz
-// review opisów poleceń w kodzie
-// grep za Storage (powinno być deep archive)
